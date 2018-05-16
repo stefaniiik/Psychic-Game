b1d872d6e66a370e2c07cf79e5915fde766cb173
unknown <stefanikrautstrunk@gmail.com>
2018-05-15 18:22:44 -0700

    lenfth error corrected

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,34 +1,61 @@
+// Computer has 26 letters to choose from
 var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
 "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
+// Variables for the text in HTML
 var wins = 0;
-var losses =0;
-var numGuesses= 9;
+var losses = 0;
+var numGuesses = 9;
+var guessesLeft = 9;
 var guessChoices = [];
+var letterToGuess = null;
+// Computer selects a random letter
+var computerGuess = computerChoices [Math.floor (Math.random() * computerChoices.length)];
 
-document.onkeyup = function(event) {
-    var userGuess = event.key;
-    var computerGuess = computerChoices[Mathhfloor(Math.random() * computerChoices.length)];
-    var options =["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m",
-    "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
-    if (options.indexOf(userGuess) > -1) {
-        if (userGuess === computerGuess) {
-            wins++;
-            numGuesses = 9;
-            guesChoice = [];
-        }
-        if (numGuesses = 9);
-        losses ++;
-        guesChoices = [];
-    }
-    var html =
-    "<h1>The Psychic Game</h1>" +
-    "<p>Guess what letter I'm thinking of...</p>" +
-    "<p>Wins:" + wins + "</p>" +
-    "<p>Losses:" + losses + "</p>" +
-    "<p>Guesses Left:" + numGuesses + "</p>" +
-    "<p>Your guesses so far:" + guessChoices.join (",") + "</p>"
-
-    document.querySelector("#game").innerHTML = html;
+var updateGuessesLeft = function () {
+// Guesses left
+document.querySelector('#guessLeft').innerHTML = "Guesses left:" + guessesLeft;
 };
+var updateLetterToGuess = function () {
+    updateLetterToGuess.letterToGuess = computerChoices [Math.random() * computerChoices.length];
+} 
+    // Guesses tried
+var updateGuesses = function(){
+    document.querySelector('#let').innerHTML = "Your Guesses so far:" + guessChoices.join(',');
+};
+// reset
+var reset = function () {
+    numGuesses = 9;
+    guessesLeft = 9;    
+    guessChoices = [];
+
+    updateLetterToGuess();
+    updateGuessesLeft();
+    updateGuesses();
+}
+
+    updateLetterToGuess ();
+    updateGuessesLeft();
+
+// Key released function
+document.onekeyup = function(event) {
+    guessesLeft--;
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+
+    guessedLetters.push(userGuess);
+    updateGuessesLeft();
+    updateGuesses();
+    // if correct letter guessed - wins go up
+        if (guessesLeft > 0) {
+            if (userGuess == letterToGuess){
+                wins++;
+                document.querySelector('#wins').innerHTML = "Wins:" + wins;
+                resent();
+            }
+        }
+        // if wrong letter guessed - losses go up
+        else if (guessesLeft == 0){
+            losses++;
+            document.querySelector('#losses').innerHTML = "Losses:" + losses;
+            reset();
+        }
+        };
